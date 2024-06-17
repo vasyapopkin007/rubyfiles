0d22ad333e874f6d009ee15e142bfedb15b3b713
vasyapopkin007 <kmnnzkh@gmail.com>
2024-06-17 15:04:23 +0300

    ddd hhh

--- ddd.rb
+++ ddd.rb
@@ -1,35 +1,43 @@
 
-
+loop do
     puts "напишите ваше любимое число"
-     x = gets.chomp.to_i
-    if x.class != Integer
+     x = gets.chomp
+    if x.match(/^[0-9]+$/)
+        x = x.to_i
         x += 1
-        puts "хотите ли вы увидеть своё новое число? y/n"
-        g = gets.chomp
-        if g == "n"
-            loop do
-                puts "неправльный ответ"
-                puts "хотите ли вы увидеть своё новое число? y/n"
-                g = gets.chomp
-                break if g == "y" or g == "yes"
-            end
-        elsif g == "no"
-            loop do
-                puts "неправльный ответ"
-                puts "хотите ли вы увидеть своё новое число? y/n"
-                g = gets.chomp
-                break if g == "y"
-                end
-        elsif g == "y"
-            puts "ваше новое любимое число #{x}"
-            return
-        elsif g == "yes"
-            puts "ваше новое любимое число #{x}"
-            return
+        loop do
+            puts "хотите ли вы увидеть своё новое число? y/n"
+            g = gets.chomp
+            if g == "n"
+                # loop do
+                    puts "неправльный ответ"
+                    next
+                    # puts "хотите ли вы увидеть своё новое число? y/n"
+                    # g = gets.chomp
+                    # break if g == "y" or g == "yes"
+                # end
+            elsif g == "no"
+                # loop do
+                    puts "неправльный ответ"
+                    next
+                    # puts "хотите ли вы увидеть своё новое число? y/n"
+                    # g = gets.chomp
+                    # break if g == "y"
+                    # end
+            elsif g == "y"
+                puts "ваше новое любимое число #{x}"
+                return
+            elsif g == "yes"
+                puts "ваше новое любимое число #{x}"
+                return
             else
-            puts "попоробуйте ещё раз"
+                puts "попоробуйте ещё раз"
+            end
+            break
         end
     else
      puts "попоробуйте ещё раз"
+     next
     end
-    код не работает, на ввод цифр считает что цифры это не integer
+    break
+end
